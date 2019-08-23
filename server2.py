3f0e57a3fd241820bdd3ecbcf9103cf1074b5bec
IGUO <ribo_email@126.com>
2019-08-23 17:09:47 +0800

    20190823下班前第二次提交（pyecharts生成html然后render_template()调用

--- server2.py
+++ server2.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from jinja2 import Markup, Environment, FileSystemLoader
 from pyecharts.globals import CurrentConfig
 
@@ -17,11 +17,13 @@ app = Flask(__name__, static_folder="templates")
 
 def bar_base() -> Bar:
     c = (
-        Bar(init_opts=opts.InitOpts(theme=ThemeType.WESTEROS, width="1080px", height="700px"))
+        Bar(init_opts=opts.InitOpts(theme=ThemeType.CHALK, width="1500px", height="700px"))
         .add_xaxis(["一室", "二室", "三室", "四室", "五室", "六室", "七室", "八室", "十室", "十一室", "十三室", "十五室"])
         .add_yaxis("人数", [randrange(0, 100) for _ in range(12)])
         .add_yaxis("评分", [randrange(0, 100) for _ in range(12)])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-我喔藕我我我我", subtitle="副标题"))
+        .add_yaxis("月评分",[randrange(0, 100) for _ in range(12)])
+        .add_yaxis("年评分", [randrange(0, 100) for _ in range(12)])
+        .set_global_opts(title_opts=opts.TitleOpts(title="Bar图", subtitle="研究室月度评分"))
     )
     return c
 
@@ -29,9 +31,12 @@ def bar_base() -> Bar:
 @app.route("/")
 def index():
     c = bar_base()
-    print('woooooo就i偶偶')
-    return Markup(c.render_embed())
+    print('IGUO server is connecting')
+#    return Markup(c.render_embed()) 跑templates里的html
+#    return c.dump_options_with_quotes()  # 返回json字符
+    c.render("D://JavaScriptData//BS_Flask2//templates//my.html")  # 生成html在tempaltes里
+    return render_template("my.html")  # 驱动templates里的html
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9000)
+    app.run()
